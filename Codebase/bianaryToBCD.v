e93dad49e87f24774dd8b87614d35db1a221845f
lucmaxwell <lucmaxwell8@gmail.com>
2021-11-05 23:23:47 -0500

    Updated BCD

--- Codebase/bianaryToBCD.v
+++ Codebase/bianaryToBCD.v
@@ -1,13 +1,14 @@
-module bianaryToBCD(
-	input [4:0] bianary,
-	output reg [3:0] ones,
-	output reg [3:0] tens);
-	
+module bianaryToBCD
+	(
+		input [9:0] bianary,
+		output reg [3:0] ones, tens, hundreds, thousands
+	);
+
 	always @(bianary) begin
-	
 		ones = bianary % 10;
-		tens = (bianary - ones) / 10;
-		
+		tens = ((bianary - ones) / 10) % 10;
+		hundreds = ((bianary - tens) / 10) % 10;
+		thousands = ((bianary - hundreds) / 10) % 10;
 	end
 	
 endmodule
