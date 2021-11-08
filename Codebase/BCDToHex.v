55f18dfdc9454494ecae3d6b97b052e43503aa4f
arunjbr <93297117+AeronBalbin@users.noreply.github.com>
2021-11-08 15:38:41 -0600

    BinarytoBCD, and BCDtoHEX modules

--- Codebase/BCDToHex.v
+++ Codebase/BCDToHex.v
@@ -1,4 +1,5 @@
 module BCDToHex(
+	input en,
 	input [3:0] BCD,
 	output reg [6:0] sevenSeg);
 	
@@ -8,20 +9,19 @@ module BCDToHex(
 	*/
 	
 	always @(BCD) begin
-		
-		case(BCD)
-		
-			4'b0000: sevenSeg = 7'b1000000; // 0
-			4'b0001: sevenSeg = 7'b1111001; // 1
-			4'b0010: sevenSeg = 7'b0100100; // 2
-			4'b0011: sevenSeg = 7'b0110000; // 3
-			4'b0100: sevenSeg = 7'b0011001; // 4
-			4'b0101: sevenSeg = 7'b0010010; // 5
-			4'b0110: sevenSeg = 7'b0000010; // 6
-			4'b0111: sevenSeg = 7'b1111000; // 7
-			4'b1000: sevenSeg = 7'b0000000; // 8
-			4'b1001: sevenSeg = 7'b0010000; // 9
-			default: sevenSeg = 7'b1111111; // Shows nothing
+		if (en == 0);
+			case(BCD)
+				4'b0000: sevenSeg = 7'b1000000; // 0
+				4'b0001: sevenSeg = 7'b1111001; // 1
+				4'b0010: sevenSeg = 7'b0100100; // 2
+				4'b0011: sevenSeg = 7'b0110000; // 3
+				4'b0100: sevenSeg = 7'b0011001; // 4
+				4'b0101: sevenSeg = 7'b0010010; // 5
+				4'b0110: sevenSeg = 7'b0000010; // 6
+				4'b0111: sevenSeg = 7'b1111000; // 7
+				4'b1000: sevenSeg = 7'b0000000; // 8
+				4'b1001: sevenSeg = 7'b0010000; // 9
+				default: sevenSeg = 7'b1111111; // Shows nothing
 			
 		endcase
 		
