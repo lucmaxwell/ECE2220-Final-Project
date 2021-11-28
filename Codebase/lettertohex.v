4899db0118938e77e4070c059d9505996d1f6c43
lucmaxwell <lucmaxwell8@gmail.com>
2021-11-27 20:24:16 -0600

    Updated BCD codes

--- Codebase/lettertohex.v
+++ Codebase/lettertohex.v
@@ -9,14 +9,14 @@ module lettertohex(sevbits, sevseg);
        sevseg = 7'b0001000;
    else if(sevbits == 8'b00110010) //32 - B
        sevseg = 7'b0000011;
-   else if(sevbits == 8'b00100011) //21 - C
-       sevseg = 7'b0110001;
+   else if(sevbits == 8'b00100001) //21 - C
+       sevseg = 7'b1000110;
    else if(sevbits == 8'b00100011) //23 - D
-       sevseg = 7'b1000010;
+       sevseg = 7'b0100001;
    else if(sevbits == 8'b00100100) //24 - E
        sevseg = 7'b0000110;
-   else if(sevbits == 8'b00101000) //2B - F
-       sevseg = 7'b0111000;
+   else if(sevbits == 8'b00101011) //2B - F
+       sevseg = 7'b0001110;
    else if(sevbits == 8'b00110100) //34 - G
        sevseg = 7'b1000010;
    else if(sevbits == 8'b00110011) //33 - H 
@@ -26,37 +26,37 @@ module lettertohex(sevbits, sevseg);
    else if(sevbits == 8'b00111011) //3B - J
        sevseg = 7'b1100001;
    else if(sevbits == 8'b01000010) //42 - K
-       sevseg = 7'b0101000;
+       sevseg = 7'b0001010;
    else if(sevbits == 8'b01001011) //4B - L
        sevseg = 7'b1000111;
    else if(sevbits == 8'b00111010) //3A - M
        sevseg = 7'b0101010;
    else if(sevbits == 8'b00110001) //31 - N
-       sevseg = 7'b1101010;
+       sevseg = 7'b0101011;
    else if(sevbits == 8'b01000100) //44 - O
-       sevseg = 7'b1100010;
+       sevseg = 7'b0100011;
    else if(sevbits == 8'b01001101) //4D - P
-       sevseg = 7'b0011000;
-   else if(sevbits == 8'b00010101) //15 - Q
        sevseg = 7'b0001100;
+   else if(sevbits == 8'b00010101) //15 - Q
+       sevseg = 7'b0011000;
    else if(sevbits == 8'b00101101) //2D - R
-       sevseg = 7'b1111010;
+       sevseg = 7'b0101111;
    else if(sevbits == 8'b00011011) //1B - S
-       sevseg = 7'b1100111;
+       sevseg = 7'b1110011;
    else if(sevbits == 8'b00101100) //2C - T
-       sevseg = 7'b1110000;
+       sevseg = 7'b0000111;
    else if(sevbits == 8'b00111100) //3C - U
-       sevseg = 7'b1000001;
+       sevseg = 7'b1100011;
    else if(sevbits == 8'b00101010) //2A - V
        sevseg = 7'b1100111;
    else if(sevbits == 8'b00011101) //1D - W
        sevseg = 7'b1010101;
    else if(sevbits == 8'b00100010) //22 - X
-       sevseg = 7'b1110110;
+       sevseg = 7'b0110110;
 	else if(sevbits == 8'b00110101) //35 - Y
-       sevseg = 7'b1000100;
+       sevseg = 7'b0010001;
    else if(sevbits == 8'b00011010) //1A - Z
-       sevseg = 7'b0010010;
+       sevseg = 7'b0110111;
 		 // Numbers
    else if(sevbits == 8'b00010110) 
        sevseg = 7'b1111001;//1
